ee5ffd7c3f522b72da6db80dc8f73c02f3a89c72
Sahil Rajput <shailkumar.ssm.fiitjee@gmail.com>
2020-09-10 13:51:53 +0530

    updates

---
+++ Controllers/ValuesCheckController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleProjectSahil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesCheckController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getVal")]
+
+        public string getdetails()
+            {
+            return "My Name is Sahil";
+            }
+    }
+}
